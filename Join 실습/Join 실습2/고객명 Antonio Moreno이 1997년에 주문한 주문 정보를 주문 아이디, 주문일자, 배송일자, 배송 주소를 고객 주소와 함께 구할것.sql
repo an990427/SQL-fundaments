8481be2f1688ea8fd6bc3620a2a1ff7193ed0346
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-02 22:01:48 +0900

    Delete 고객명 Antonio Moreno이 1997년에 주문한 주문 정보를 주문 아이디, 주문일자, 배송일자, 배송 주소를 고객 주소와 함께 구할것.sql

--- Join 실습/Join 실습2/고객명 Antonio Moreno이 1997년에 주문한 주문 정보를 주문 아이디, 주문일자, 배송일자, 배송 주소를 고객 주소와 함께 구할것.sql
+++
@@ -1,5 +0,0 @@
-select a.contact_name, a.address, b.order_id, b.order_date, b.shipped_date, b.ship_address
-from nw.customers a
-	join nw.orders b on a.customer_id = b.customer_id
-where a.contact_name = 'Antonio Moreno'
-and b.order_date between to_date('19970101', 'yyyymmdd') and to_date('19971231', 'yyyymmdd');
