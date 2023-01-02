87673d8c38b0e210cd7d9dd5176c113d4a2f79f5
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-02 22:07:15 +0900

    Create Madrid에 살고 있는 고객이 주문한 주문 정보를 구할것.sql

---
+++ Join 실습/Outer join/Madrid에 살고 있는 고객이 주문한 주문 정보를 구할것.sql
@@ -0,0 +1,9 @@
+-- 고객명, 주문id, 주문일자, 주문접수 직원명, 배송업체명을 구하되, 
+-- 만일 고객이 주문을 한번도 하지 않은 경우라도 고객정보는 빠지면 안됨. 이경우 주문 정보가 없으면 주문id를 0으로 나머지는 Null로 구할것. 
+select a.customer_id, a.contact_name, coalesce(b.order_id, 0) as order_id, b.order_date
+	, c.first_name||' '||c.last_name as employee_name, d.company_name as shipper_name  
+from nw.customers a
+	left join nw.orders b on a.customer_id = b.customer_id
+	left join nw.employees c on b.employee_id = c.employee_id
+	left join nw.shippers d on b.ship_via = d.shipper_id
+where a.city = 'Madrid';
