9247246f0a74f1bd94b02b1fd6a474bf984b3d60
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-02 22:01:35 +0900

    Delete Beverages 카테고리에 속하는 모든 상품아이디와 상품명, 그리고 이들 상품을 제공하는 supplier 회사명 정보 구할것.sql

--- Join 실습/Join 실습2/Beverages 카테고리에 속하는 모든 상품아이디와 상품명, 그리고 이들 상품을 제공하는 supplier 회사명 정보 구할것.sql
+++
@@ -1,5 +0,0 @@
-select a.category_id, a.category_name, b.product_id, b.product_name, c.supplier_id, c.company_name 
-from nw.categories a
-	join nw.products b on a.category_id = b.category_id
-	join nw.suppliers c on b.supplier_id = c.supplier_id
-where category_name = 'Beverages';
