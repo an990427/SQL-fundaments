57a25658faff74224d4091fef90ce9c2253cac0e
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-02 21:30:34 +0900

    Create 부서명 SALES와 RESEARCH의 소속 직원들의 부서명, 직원번호, 직원명, JOB 그리고 과거 급여 정보 추출.sql

---
+++ Join 실습/부서명 SALES와 RESEARCH의 소속 직원들의 부서명, 직원번호, 직원명, JOB 그리고 과거 급여 정보 추출.sql
@@ -0,0 +1,6 @@
+select a.dname, b.empno, b.ename, b.job, c.fromdate, c.todate, c.sal 
+from hr.dept a
+	join hr.emp b on a.deptno = b.deptno
+	join hr.emp_salary_hist c on b.empno = c.empno
+where a.dname in('SALES', 'RESEARCH')
+order by a.dname, b.empno, c.fromdate;
