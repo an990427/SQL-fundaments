c2a8df99a0551b70efac934e46286e3350b43cc1
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-04 22:46:05 +0900

    Create 상품 카테고리 + 상품별 + 주문처리직원별 매출.sql

---
+++ Group by 실습/Cube/상품 카테고리 + 상품별 + 주문처리직원별 매출.sql
@@ -0,0 +1,8 @@
+select c.category_name, b.product_name, e.last_name||e.first_name as emp_name, sum(amount) 
+from nw.order_items a
+	join nw.products b on a.product_id = b.product_id
+	join nw.categories c on b.category_id = c.category_id
+	join nw.orders d on a.order_id = d.order_id
+	join nw.employees e on d.employee_id = e.employee_id
+group by c.category_name, b.product_name, e.last_name||e.first_name
+order by 1, 2, 3;
