74b1e020c8cf014c0f0861026c9593460bf74484
Jaejun An <50936042+an990427@users.noreply.github.com>
2023-01-04 22:42:56 +0900

    Create group by Pivoting시 조건에 따른 건수 계산 유형(count case when then 1 else null end).sql

---
+++ Group by 실습/Group by와 Aggregate 함수의 case when 을 이용한 pivoting/group by Pivoting시 조건에 따른 건수 계산 유형(count case when then 1 else null end).sql
@@ -0,0 +1,8 @@
+select deptno, count(*) as cnt
+	, count(case when job = 'SALESMAN' then 1 end) as sales_cnt
+	, count(case when job = 'MANAGER' then 1 end) as manager_cnt
+	, count(case when job = 'ANALYST' then 1 end) as analyst_cnt
+	, count(case when job = 'CLERK' then 1 end) as clerk_cnt
+	, count(case when job = 'PRESIDENT' then 1 end) as president_cnt
+from emp
+group by deptno;
